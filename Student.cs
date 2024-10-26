828bc723deb2607b39229b84cf79db1805da4207
TomasSteifo <Steifotomas@gmail.com>
2024-10-26 13:30:55 +0200

    Add project files.

---
+++ Student.cs
@@ -0,0 +1,16 @@
+﻿ public class Student
+ {
+    public string Name {  get; set; }
+    public string StudentId { get; set; }   
+    public int Age { get; set; }
+    public Dictionary<string, List <int>> GradesBySubject { get; set; }
+
+    public Student(string name, string id, int age) 
+    { 
+        Name = name;
+        StudentId = id;
+        Age = age;
+        GradesBySubject = new Dictionary<string, List <int>>();
+    }
+ }
+
